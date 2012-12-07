6466956a3c79b840159c139ea15040e896ad1ba9
Hjalte J <skjoldborg94@edb.dk>
2012-12-07 15:03:32 +0100

    Documentations

--- src/spreadsheet/CellReference.java
+++ src/spreadsheet/CellReference.java
@@ -1,24 +1,41 @@
 package spreadsheet;
-
+/**
+ * 
+ * CellReference extends Expression
+ */
 public final class CellReference extends Expression {
 	
 	private Spreadsheet sheet;
 	private Position pos;
 	
-	public CellReference (final Spreadsheet s, final Position p) {
-		   sheet = s;
-		   pos = p;
+	/**
+	 * Stores a reference in a Spreadsheet with a non null Position
+	 * @param spread a Spreadsheet
+	 * @param position a non null Position
+	 */
+	public CellReference (final Spreadsheet spread, final Position position) {
+		   sheet = spread;
+		   pos = position;
 	}
-	
+
 	@Override
+	/**
+	 * @return the boolean Expression which the CellReference refers to.
+	 */
 	public  boolean toBoolean() {
 		  return sheet.get(pos).toBoolean();
 	}
 	@Override
+	/**
+	 * @return the Integer Expression which the CellReference refers to.
+	 */
 	public int toInt() {
 			return sheet.get(pos).toInt();
 	}
 	@Override
+	/**
+	 * @return the String Expression which the CellReference refers to.
+	 */
 	public String toString() {
 			return sheet.get(pos).toString();
 	}
--- src/spreadsheet/Expression.java
+++ src/spreadsheet/Expression.java
@@ -1,5 +1,11 @@
 package spreadsheet; 
 
+/**
+ * 
+ * Abstract class for Expressions of the type 
+ * Integer, Boolean and String
+ * Holds 3 abstract methods for converting the different types
+ */
 public abstract class Expression {
 	
 	public abstract boolean toBoolean();
