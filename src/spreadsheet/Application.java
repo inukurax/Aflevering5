fe08b23c1bd47dba41f6d75a89fdbe6760ec45dd
Hjalte J <skjoldborg94@edb.dk>
2012-12-17 22:43:29 +0100

    methods done

--- src/spreadsheet/Application.java
+++ src/spreadsheet/Application.java
@@ -1,22 +1,38 @@
 package spreadsheet;
 
+import java.util.ArrayList;
+
+/** A singleton class representing a spreadsheet application.
+ *
+ * The instance is initialized on first mention of the class.
+ */
 public final class Application {
+
+  private ArrayList<Spreadsheet> spreadsheets;
+  private Spreadsheet worksheet;
+
+  public static final Application instance = new Application();
+
+  private Application() {
+    this.worksheet = new Spreadsheet();
+    this.spreadsheets = new ArrayList<Spreadsheet>();
+    this.spreadsheets.add(this.worksheet);
+  }
 	
 	/**
 	 * returns the current worksheet;
 	 *  guaranteed to be be not null;
 	 * @return
 	 */
-	Spreadsheet getWorksheet() {
-		return null;
-		
+	public Spreadsheet getWorksheet() {
+		return worksheet;
 	}
 	/**
 	 *  initializes a new empty spreadsheet; 
 	 *  the worksheet is retained;
 	 */
-	void newSpreadsheet() {
-		
+	public void newSpreadsheet() {
+	    this.spreadsheets.add(new Spreadsheet());
 	}
 	
 	/**
@@ -24,8 +40,20 @@ public final class Application {
 	 *  throws NoSuchSpreadsheetException 
 	 *  if there is no spreadsheet by the specified name;
 	 * @param name
+	 * @throws NoSuchSpreadsheetException 
 	 */
-	void changeWorksheet(final String name) {
+	public void changeWorksheet(final String name) 
+			      throws NoSuchSpreadsheetException {
+		if (worksheet.getName().equals(name))
+			return;
+		
+		for (Spreadsheet sheet : spreadsheets) {
+			if (sheet.getName().equals(name)) {
+				this.worksheet = sheet;
+				return;
+			}
+		}
+		throw new NoSuchSpreadsheetException(name);
 		
 	}
 	
@@ -35,10 +63,16 @@ public final class Application {
 	 * if there is no spreadsheet by the specified name;
 	 * @param name
 	 * @return
+	 * @throws NoSuchSpreadsheetException 
 	 */
-	Spreadsheet getSpreadsheet(final String name) {
-		return null;
+	public Spreadsheet getSpreadsheet(final String name) 
+						throws NoSuchSpreadsheetException {
 		
+		for (Spreadsheet sheet : spreadsheets) {
+			if (sheet.getName().equals(name))
+				return sheet;
+		}
+		throw new NoSuchSpreadsheetException(name);
 	}
 	
 	/**
@@ -46,14 +80,14 @@ public final class Application {
 	 * @return
 	 */
 	Iterable<String> listSpreadsheets() {
-		return null;
+		return (Iterable<String>) spreadsheets.listIterator();
 		
 	}
 	/**
 	 * exits the application with exit code 0.
 	 */
-	void exit() {
-		
+	public void exit() {
+		System.exit(0);
 	}
 
 }
