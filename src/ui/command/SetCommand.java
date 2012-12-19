393e77f4f2f6e24d019359f0c87c1dea3f0b9cd0
Hjalte J <skjoldborg94@edb.dk>
2012-12-19 18:36:26 +0100

    getType

--- src/ui/command/SetCommand.java
+++ src/ui/command/SetCommand.java
@@ -1,11 +1,13 @@
 package ui.command;
 
-import java.beans.Expression;
 
 import spreadsheet.Application;
 import spreadsheet.NoSuchSpreadsheetException;
 import spreadsheet.Position;
-import spreadsheet.arithmetic.AConst;
+import spreadsheet.arithmetic.*;
+import spreadsheet.logical.*;
+import spreadsheet.textual.*;
+import spreadsheet.*;
 
 public final class SetCommand extends Command {
 
@@ -32,7 +34,18 @@ public final class SetCommand extends Command {
 		Position position = new Position(argInt1, argInt2);
 		AConst expression = new AConst(2);
 		Application.instance.getWorksheet().set(position, expression);
-		System.out.println(String.format("pos (%d,%d) exp: %s + rest:%s", argInt1,argInt2,expType, arguments));
+		System.out.println(String.format("set new %s(%s) at Position(%d,%d)",expType, arguments,argInt1,argInt2));
+	}
+	
+	private Expression getType(String str) {
+		switch (str) {
+		case "AConst" : return new AConst(0);
+		case "Add" : return new Add(null,null);
+		case "Neg" : return new Neg(null);
+
+		}
+		return null;
+		
 	}
 
 }
